db5fab055ea1af734e83213262d5c2ac486d76df
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-05-01 09:36:56 +0200

    chore: configure config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,52 +1,60 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Server Config",
-  description: "A VitePress Site",
-  lastUpdated: true,
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-
-        ]
-      },
-        {
-          text: "Steps",
-          items: [
-            { text: '00_cloudflare_dns', link: '/00_cloudflare_dns' },
-            { text: '01_add_unprivileged_user', link: '/01_add_unprivileged_user' },
-            { text: '02_ssh_keys', link: '/02_ssh_keys' },
-            { text: '03_ssh_security_settings', link: '/03_ssh_security_settings' },
-            { text: '04_ssh_totp', link: '/04_ssh_totp' },
-            { text: '05_msmtp', link: '/05_msmtp' },
-            { text: '06_auto_updates', link: '/06_auto_updates' },
-            { text: '07_install_software', link: '/07_install_software' },
-            { text: '08_ufw', link: '/08_ufw' },
-            { text: '09_ufw_cloudflare', link: '/09_ufw_cloudflare' },
-            { text: '10_psad', link: '/10_psad' },
-            { text: '11_fail2ban', link: '/11_fail2ban' },
-            { text: '12_bash_config', link: '/12_bash_config' },
-            { text: '13_wireguard', link: '/13_wireguard' },
-            { text: '14_docker', link: '/14_docker' },
-            { text: '15_docker_network', link: '/15_docker_network' },
-            { text: '16_portainer', link: '/16_portainer' },
-            { text: '17_nginx_proxy_manager', link: '/17_nginx_proxy_manager' },
-            { text: '18_flame', link: '/18_flame' },
-            { text: '19_watchtower', link: '/19_watchtower' },
-          ]
-        }
-    ],
-
-  }
+    title: "Server Config",
+    description: "A VitePress Site",
+    lastUpdated: true,
+    base: '/server-config/',
+//	markdown: {
+//		lineNumbers: true,
+//	},
+    themeConfig: {
+        aside: true,
+        outline: [2, 3],
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'},
+        ],
+        footer: {
+            message: 'Created by nikita-t1.',
+//            copyright: ''
+        },
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Setup', link: '/steps/overview'},
+            {text: 'Container', link: '/container/overview'},
+        ],
+        sidebar: {
+            '/steps/': [
+                {
+                    text: "Steps",
+                    items: [
+                        {text: 'Overview', link: '/steps/overview'},
+                        {text: 'Cloudflare DNS', link: '/steps/00_cloudflare_dns'},
+                        {text: 'Unprivileged User', link: '/steps/01_add_unprivileged_user'},
+                        {text: 'Login with SSH Keys', link: '/steps/02_ssh_keys'},
+                        {text: 'Configure SSH Settings', link: '/steps/03_ssh_security_settings'},
+                        {text: 'MFA for SSH', link: '/steps/04_ssh_totp'},
+                        {text: 'SMTP Mail Server', link: '/steps/05_msmtp'},
+                        {text: 'Unattended Updates', link: '/steps/06_auto_updates'},
+                        {text: 'Awesome Shell', link: '/steps/07_install_software'},
+                        {text: 'Uncomplicated Firewall', link: '/steps/08_ufw'},
+                        {text: 'Cloudflare WAF', link: '/steps/09_ufw_cloudflare'},
+                        {text: 'Port Scan Attack Detector', link: '/steps/10_psad'},
+                        {text: 'Fail2ban', link: '/steps/11_fail2ban'},
+                        {text: '.bashrc File', link: '/steps/12_bash_config'},
+                        {text: 'WireGuard VPN', link: '/steps/13_wireguard'},
+                        {text: 'Docker', link: '/steps/14_docker'},
+                        {text: 'Docker Network', link: '/steps/15_docker_network'},
+                        {text: 'Portainer', link: '/steps/16_portainer'},
+                    ],
+                },
+                {
+                    text: "Bash Script",
+                    link: "/bash_script",
+                },
+            ],
+        },
+    },
 })
+
